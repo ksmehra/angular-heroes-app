709d43587d63be504847ac9075f7d27a64f6da6b
k s mehra <ksheer.mehra@AAGB1737WLVDM.aaspl-brd.com>
2020-01-06 15:11:20 +0000

    interim commit; chapter 2, before click binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,9 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'windstorm'
-  };
+  }
+
+  heroes= HEROES;
 
   constructor() { }
 
